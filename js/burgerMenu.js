e7ca4cd8fc031bff9ac617e9ff1f67b0efee478e
mauri <maurice.klein16@icloud.com>
2024-09-17 20:41:34 +0200

    kleine Korrektur von doppelung

--- js/burgerMenu.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const burgerMenu = document.querySelector('.burger-menu');
-    const dropdownMenu = document.querySelector('.dropdown-menu');
-
-    // Toggle das Dropdown-Menü beim Klick auf das Burger-Menü
-    burgerMenu.addEventListener('click', function() {
-        dropdownMenu.classList.toggle('active');
-    });
-});
